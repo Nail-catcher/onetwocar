0f5f2790bd639bcfeed6a5c554cc35ca104a4a43
Nail-cathcer <nikita-mokin@mail.ru>
2021-11-11 11:52:30 +0300

    set

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -21,10 +21,12 @@
                                 <td>{{$order->botUser->name ?? null}}</td>
 
                        <td>
-                           @if(!empty($order->car->title))
-                           <a href="/cars/{{$order->car->id ?? }}">Автомобиль: {{$order->car->title}}</a>
-                               @elseif(!empty($order->set->title))
+                           @if($order->car->title)
+                           <a href="/cars/{{$order->car->id ?? null}}">Автомобиль: {{$order->car->title}}</a>
+                               @elseif($order->set->title)
                                Модель: {{$order->set->model->title}} Комплектация: {{$order->set->title}}
+                               @else
+                               Нет данных
                                @endif
                        </td>
 
